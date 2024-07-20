8663292ddb369f5b1043f2b0580624ec064b7721
HAVOK <alireza.karampoor@gmail.com>
2024-07-21 00:53:03 +0330

    feat refactor

--- components.go
+++ components.go
@@ -2,45 +2,41 @@ package main
 
 import (
 	"bytes"
-	"fmt"
-	"github.com/gin-gonic/gin"
-	"html/template"
-	"htmx/htmx"
 	"io"
+
+	"github.com/gin-gonic/gin"
 )
 
 type Htmx map[string]any
 
-func RenderComponent(name string, params gin.H, htmx Htmx, callbacks ...*htmx.Callback) template.HTML {
-	attr := make([]template.HTMLAttr, 0, len(htmx))
-	for k, v := range htmx {
-		attr = append(attr, template.HTMLAttr(fmt.Sprintf("%s=\"%s\"", k, v)))
-	}
-
-	for _, v := range callbacks {
-		attr = append(attr, template.HTMLAttr(fmt.Sprintf("%s=\"%s\"", v.Event, v.Code)))
-	}
-
-	params["htmx"] = attr
-	buff := &bytes.Buffer{}
-	err := Templates.ExecuteTemplate(buff, name, params)
+func RenderIndex(body string, c *gin.Context) error {
+	err := Templates.ExecuteTemplate(
+		c.Writer, "index", gin.H{
+			"body": body,
+		},
+	)
 	if err != nil {
-		fmt.Println(err)
-		return template.HTML(fmt.Sprintf("Error while rendering %s", name))
+		return err
 	}
-	return template.HTML(buff.String())
+	return nil
 }
 
-func RenderToBody(c *gin.Context, html template.HTML) {
-	err := Templates.ExecuteTemplate(c.Writer, "index", gin.H{
-		"Body": html,
-	})
-	if err != nil {
-		panic(err)
+func RenderComp(name string, params gin.H, htmx Htmx) string {
+	buf := bytes.Buffer{}
+	if params != nil && htmx != nil {
+		params["htmx"] = htmx
 	}
+	err := Templates.ExecuteTemplate(&buf, name, params)
+	if err != nil {
+		return err.Error()
+	}
+	return buf.String()
 }
 
-func Render(c *gin.Context, html template.HTML) error {
-	_, err := io.WriteString(c.Writer, string(html))
-	return err
+func Render(c *gin.Context, res string) error {
+	_, err := io.WriteString(c.Writer, res)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -1,35 +1,34 @@
 package main
 
 import (
+	"text/template"
+
 	"github.com/gin-gonic/gin"
-	"html/template"
-	"htmx/htmx"
+	"htmx/errors"
 )
 
 var Templates *template.Template
 
 func main() {
-	glob, err := template.ParseGlob("./templates/*.gohtml")
+	Templates = template.New("button")
+	_, err := Templates.ParseGlob("./templates/*.gohtml")
 	if err != nil {
 		panic(err)
 	}
-	Templates = glob
 
 	r := gin.Default()
 	r.Static("/static", "./static")
 
-	r.GET("/", func(c *gin.Context) {
-		RenderToBody(c, RenderComponent("button", gin.H{}, Htmx{
-			htmx.HxGet:     "/empty",
-			htmx.HxSwap:    htmx.OuterHTML,
-			htmx.HxPushUrl: "true",
-		}, htmx.OnJs(htmx.Click, "./static/index.js")))
-		return
-	})
-
-	r.GET("/empty", func(c *gin.Context) {
-		Render(c, RenderComponent("empty", gin.H{}, Htmx{}))
-	})
+	r.GET(
+		"/", func(c *gin.Context) {
+			err := RenderIndex(RenderComp("button", nil, nil), c)
+			if err != nil {
+				errors.InternalServerError(c, err)
+				return
+			}
+			return
+		},
+	)
 
 	err = r.Run("0.0.0.0:8080")
 	if err != nil {
