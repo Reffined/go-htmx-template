107b60cf2a8b547c221f6daf1b69f9640db05f1f
havok <alireza.karampoor@gmail.com>
2024-07-22 12:59:45 +0330

    init

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"htmx/htmx"
 	"text/template"
 
 	"github.com/gin-gonic/gin"
@@ -19,44 +17,6 @@ func main() {
 
 	r := gin.Default()
 	r.Static("/static", "./static")
-
-	r.GET(
-		"/", func(c *gin.Context) {
-			body := RenderComp("login", Params{
-				"MakeHtmx": MakeHtmx,
-				"htmx": Htmx{
-					htmx.HxPost:   "/empty",
-					htmx.HxTarget: "body",
-				},
-				"id": "login",
-				"submit": Params{
-					"MakeHtmx": MakeHtmx,
-					"htmx":     Htmx{},
-					"formId":   "login",
-					"class":    "flg1",
-				},
-			})
-
-			err := RenderIndex(body, c)
-			if err != nil {
-				panic(err)
-			}
-		},
-	)
-
-	r.POST(
-		"/empty", func(c *gin.Context) {
-			email, ok := c.GetPostForm("email")
-			if ok {
-				fmt.Println(email)
-			}
-
-			err := Render(c, RenderComp("empty", Params{"txt": email}))
-			if err != nil {
-				panic(err)
-			}
-		},
-	)
 
 	err = r.Run("0.0.0.0:8080")
 	if err != nil {
